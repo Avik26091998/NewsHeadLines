b39cb747644d33670c68e3d11990c71330d2dd98
avik26091998 <avik26091998@gmail.com>
2019-10-02 12:44:08 +0530

    Using GET

--- NewsHeadLines.py
+++ NewsHeadLines.py
@@ -18,9 +18,11 @@
 
 
 
-import feedparser   
-from flask import Flask
-from flask import render_template
+import feedparser   # feedparser module
+from flask import Flask  # importing Flask from flask module
+from flask import render_template # for rendering jinja logic
+from flask import request # It provides a global context which our code can use to access information about the latest request made to our application
+# The GET value's are available in requests.args
 app = Flask(__name__)
 
 RSS_FEED = {
@@ -31,14 +33,26 @@ RSS_FEED = {
 }
 
 @app.route("/")
-@app.route("/<publication>") #if we specify a part of our URL path in angle brackets <>, then it is taken as a variable and is passed to our application code...Ex - localhost:5000/cnn(Here publication=cnn).
-def get_news(publication="bbc"): # Default publication is bbc
+#@app.route("/<publication>") #if we specify a part of our URL path in angle brackets <>, then it is taken as a variable and is passed to our application code...Ex - localhost:5000/cnn(Here publication=cnn).
+def get_news(): # Default publication is bbc
+	
+	#  we create an HTML form element. 
+	#  By default, this will create an HTTP GET request when submitted, 
+	#  by passing any inputs as GET arguments into the URL. 
+	#  We have a single text input which has the name publication. 
+	#  This name is important as the GET argument will use this
+	query=request.args.get('publication') # GET arguments are available in request.args
+	if not query or query not in RSS_FEED:
+		publication='bbc'
+	else:
+		publication=query.lower()
 
 	feed = feedparser.parse(RSS_FEED[publication]) # Feedparser downloads the feed, parses it and returns a dictonary
 	first_article = feed['entries'][0] # we grabbed just the first article from the feed and assigned it to a variable
 	# Build a HTML page and display the content using the particular field('title', published,...)
 	# We use placeholders
 	return render_template("home.html", articles=feed['entries']) # looks in our templates directory for a file named home.html, reads this, parses any Jinja logic, and creates an HTML string to return to the user
+	# passing article object having feed entries
 
 if __name__ == '__main__':
 	app.run(port=5000,debug=True)
